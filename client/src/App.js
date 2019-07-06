e82289b5eb72868b2d2e7a27b6588735832be8e7
unknown <sherman.jonathan.r@gmail.com>
2019-07-06 13:20:01 -0700

    uploading file structure for front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
-import Main from "./components/Main";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from "./components/LandingPage/Main";
+// import all pages here
+import LandingPage from ".pages/LandingPage";
+import NewUser from "./pages/NewUser";
+import PortfolioCreation from "./pages/PortfolioCreation";
+import PortfoliosCreated from "./pages/PortfoliosCreated";
+import Preview from "./pages/Preview";
+import UserSplash from "./pages/UserSplash";
+
 
 function App() {
   return (
-    <div>
-      <div className="jumbotron">
-        <Main />
-      </div>
-    </div>
+    // <Router>
+    //   <div>
+    //     <Switch>
+    //       <Route exact path="/" component={LandingPage} />
+    //       <Route exact path="/" component={NewUser} />
+    //       <Route exact path="/" component={PortfolioCreation} />
+    //       <Route exact path="/" component={PortfoliosCreated} />
+    //       <Route exact path="/" component={Preview} />
+    //       <Route exact path="/" component={UserSplash} />
+    //     </Switch>
+    //   </div>
+    // </Router>
   );
 }
 
