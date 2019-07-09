43a504b3b6f17fa5b1316a38cd64b0e60124d028
unknown <sherman.jonathan.r@gmail.com>
2019-07-08 19:15:03 -0700

    added teams linkedIn links

--- client/src/components/AboutUs/AboutUs.js
+++ client/src/components/AboutUs/AboutUs.js
@@ -8,7 +8,7 @@ function AboutUs() {
         <h2 className="text-center">Harrison</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praeterea sublata cognitione et scientia tollitur omnis ratio et vitae degendae et rerum gerendarum. Si enim ad populum me vocas, eum. Huius ego nunc auctoritatem sequens idem faciam. Cur igitur, cum de re conveniat, non malumus usitate loqui? Est autem etiam actio quaedam corporis, quae motus et status naturae congruentis tenet; Duo Reges: constructio interrete. Nam, ut sint illa vendibiliora, haec uberiora certe sunt. Minime vero istorum quidem, inquit. Quid ergo hoc loco intellegit honestum? Cur igitur easdem res, inquam, Peripateticis dicentibus verbum nullum est, quod non intellegatur? Quo modo autem optimum, si bonum praeterea nullum est? Ait enim se, si uratur, Quam hoc suave! dicturum.</p>
         <ul className="icons">
-          <a href="https://www.linkedin.com/in/jonathan-sherman-39090184/"
+          <a href="https://www.linkedin.com/in/harrison-fales-419585b0/ "
             className="icon fa-linkedin"><span className="label">LinkedIn</span></a>
             <br></br>
           <a href="https://github.com/harblesin" className="icon fa-github"><span
@@ -19,7 +19,7 @@ function AboutUs() {
         <h2 className="text-center">Daniel</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praeterea sublata cognitione et scientia tollitur omnis ratio et vitae degendae et rerum gerendarum. Si enim ad populum me vocas, eum. Huius ego nunc auctoritatem sequens idem faciam. Cur igitur, cum de re conveniat, non malumus usitate loqui? Est autem etiam actio quaedam corporis, quae motus et status naturae congruentis tenet; Duo Reges: constructio interrete. Nam, ut sint illa vendibiliora, haec uberiora certe sunt. Minime vero istorum quidem, inquit. Quid ergo hoc loco intellegit honestum? Cur igitur easdem res, inquam, Peripateticis dicentibus verbum nullum est, quod non intellegatur? Quo modo autem optimum, si bonum praeterea nullum est? Ait enim se, si uratur, Quam hoc suave! dicturum.</p>
         <ul className="icons">
-          <a href="https://www.linkedin.com/in/jonathan-sherman-39090184/"
+          <a href="https://www.linkedin.com/in/daniel-bryant-b3418a9b/"
             className="icon fa-linkedin"><span className="label">LinkedIn</span></a>
           <br></br>
           <a href="https://github.com/Minority85" className="icon fa-github"><span
@@ -30,7 +30,7 @@ function AboutUs() {
         <h2 className="text-center">James</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praeterea sublata cognitione et scientia tollitur omnis ratio et vitae degendae et rerum gerendarum. Si enim ad populum me vocas, eum. Huius ego nunc auctoritatem sequens idem faciam. Cur igitur, cum de re conveniat, non malumus usitate loqui? Est autem etiam actio quaedam corporis, quae motus et status naturae congruentis tenet; Duo Reges: constructio interrete. Nam, ut sint illa vendibiliora, haec uberiora certe sunt. Minime vero istorum quidem, inquit. Quid ergo hoc loco intellegit honestum? Cur igitur easdem res, inquam, Peripateticis dicentibus verbum nullum est, quod non intellegatur? Quo modo autem optimum, si bonum praeterea nullum est? Ait enim se, si uratur, Quam hoc suave! dicturum.</p>
         <ul className="icons">
-          <a href="https://www.linkedin.com/in/jonathan-sherman-39090184/"
+          <a href="https://www.linkedin.com/in/jamesrader14/"
             className="icon fa-linkedin"><span className="label">LinkedIn</span></a>
           <br></br>
           <a href="https://github.com/jamesrader" className="icon fa-github"><span
