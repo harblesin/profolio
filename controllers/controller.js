c5d2ef4f6330e666d650363733c67aba60134c22
h f <waltermelonson@gmail.com>
2019-07-09 20:58:21 -0700

    added route for grabbing based on user id through joins using the models

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,5 +7,10 @@ module.exports = {
 
   saveUser: function(req, res) {
     db.users.create(req.body).then(data => res.json(data));
+  },
+
+  createPort: function(req, res) {
+    db.users.findAll({where:{userId: req.params}}).then(data => res.json(data))
   }
+
 };
