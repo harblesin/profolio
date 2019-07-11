6a5ae297dfb1d187e3af82d095b3a665ba189791
h f <waltermelonson@gmail.com>
2019-07-10 17:36:21 -0700

    began adding bycrypt, passport, and jsonwebtokens into the routes to create sessionless authentification

--- routes/API/userRoutes.js
+++ routes/API/userRoutes.js
@@ -1,5 +1,34 @@
 const router = require("express").Router();
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const keys = require("../authSetup");
 const userController = require("../../controllers/controller");
+const User = require("../../models/User")
+
+
+router.post("/register", async (req,res)=> {
+  const { username, password } = req.body;
+
+  const hashCost = 10;
+
+  try {
+    const passwordHash = await bcrypt.hash(password, hashCost);
+    const userDocument = new UserModel({ username, passwordHash });
+    await userDocument.save()
+
+    res.status(200).send({username})
+  }
+  catch(error){
+    res.status(400).send({
+      error: "req.body isnt taking the form data"
+    })
+  }
+});
+
+router.post("/login")
+
+
 
 router
   .route("/")
