584dff83d11a2b538262f96e8ca19782a7a3cc00
ylech <waltermelonson@gmail.com>
2019-07-10 21:31:26 -0700

    changed place of json token handing out, created key page for testing, routes should work

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,8 +7,24 @@ module.exports = {
 
   saveUser: function(req, res) {
     console.log(req.body);
-    db.User.create(req.body).then(data => res.json(data));
-    db.Details.create(req.body).then(data => res.json(data));
-  }
+    async (req, res) => {
+      const { username, password } = req.body;
 
+      const hashCost = 10;
+
+      try {
+        const passwordHash = await bcrypt.hash(password, hashCost);
+        const userDocument = new UserModel({ username, passwordHash });
+        await userDocument.save();
+
+        db.User.create({ username }).then(data => res.json(data));
+        //res.status(200).send({ username });
+      } catch (error) {
+        res.status(400).send({
+          error: "req.body isnt taking the form data"
+        });
+      }
+    };
+  }
+  // db.Details.create(req.body).then(data => res.json(data));
 };
--- routes/API/userRoutes.js
+++ routes/API/userRoutes.js
@@ -4,10 +4,9 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const keys = require("../authSetup");
 const userController = require("../../controllers/controller");
-const User = require("../../models/User")
+const User = require("../../models/User");
 
-
-router.post("/register", async (req,res)=> {
+router.post("/register", async (req, res) => {
   const { username, password } = req.body;
 
   const hashCost = 10;
@@ -15,24 +14,19 @@ router.post("/register", async (req,res)=> {
   try {
     const passwordHash = await bcrypt.hash(password, hashCost);
     const userDocument = new UserModel({ username, passwordHash });
-    await userDocument.save()
+    await userDocument.save();
 
-    res.status(200).send({username})
-  }
-  catch(error){
+    res.status(200).send({ username });
+  } catch (error) {
     res.status(400).send({
       error: "req.body isnt taking the form data"
-    })
+    });
   }
 });
 
-router.post("/login")
+router.route("/register").post(userController.saveUser);
+router.post("/login");
 
-
-
-router
-  .route("/")
-  .post(userController.saveUser)
-  .get(userController.findAll);
+router.route("/").get(userController.findAll);
 
 module.exports = router;
