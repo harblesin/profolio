11003ae0cc782a57e2153c948505f3e21aff1ae1
h f <waltermelonson@gmail.com>
2019-07-25 22:48:12 -0700

    Further testing configuration for troubleshooting heroku deployment issues

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ require("./config/authSetup.js")(passport, db.User);
 app.use(cookies())
 app.use(router);
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use("/static", express.static(path.join(__dirname, "client/build")));
 }
 
 var syncOptions = { force: false };
