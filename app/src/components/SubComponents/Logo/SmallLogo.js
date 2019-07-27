52747057e91819c2680f2ea971189f74dd91f9c0
ylech <waltermelonson@gmail.com>
2019-07-27 10:11:11 -0700

    Testing

--- app/src/components/SubComponents/Logo/SmallLogo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function SmallLogo(props) {
-    return (
-        <img alt="Profolio Logo" src="../images/profolio-small.png" {...props}/>
-    )
-}
-
-export default SmallLogo;
