8779f4c30781f666ff547ece37cf31b2f8015a4e
h f <waltermelonson@gmail.com>
2019-07-28 14:47:07 -0700

    Serves final page, creation page is protected, custom url for each page for user

--- client/src/pages/finalPage.js
+++ client/src/pages/finalPage.js
@@ -11,10 +11,6 @@ import ReactDOM from "react-dom";
 import ProjectCard from "../components/SubComponents/ProjectCard/ProjectCard";
 import axios from "axios";
 import API from "../utils/API";
-import { Modal } from "react-bootstrap"
-import Form from "../components/SubComponents/Form/Form"
-import Button from "../components/SubComponents/Button/Button"
-import { Redirect } from "react-router-dom";
 
 class PortfolioCreation extends Component {
   state = {
@@ -27,7 +23,6 @@ class PortfolioCreation extends Component {
     githubLink: "Github Project Link",
     deployedLink: "Deployed Project Link",
     cardButton: "Click on this button to create a new Project Card",
-    footer: 0,
     skills: [],
     skillRemove: [],
     baseImage: "images/profile-pic-placeholder.png",
@@ -37,32 +32,13 @@ class PortfolioCreation extends Component {
     footerTruth: false,
     eachProject: [],
     savedProject: [],
-    deleteProjectButton: "Delete Button",
-    servedLink: "",
-    show: false,
-    redirect: false
+    deleteProjectButton: "Delete Button"
   };
-
-    renderRedirect = () => {
-      if (this.state.redirect) {
-        return <Redirect to="/" />;
-      }
-    };
-
 
   componentDidMount = () => {
     // let array = [
     //   [bio]
     // ];
-
-    API.authCheck().then(data => {
-      console.log(data.data);
-      if (!data.data.auth || data.data === "No auth token") {
-        this.setState({ redirect: true });
-      }
-    });
-
-    
 
     const {
       match: { params }
@@ -302,110 +278,12 @@ class PortfolioCreation extends Component {
     console.log(projectCard);
   };
 
-  serveLink = () => {
-    let newId = (((this.state.profolioId + 255) * 3) - 1234)
-    this.setState({
-      servedLink: newId
-    })
-    console.log(newId)
-    this.handleShow()
-  }
-
-  handleClose = () => {
-    console.log(this.state.show);
-    this.setState({ show: false });
-  };
-  handleShow = () => {
-    console.log(this.state.show);
-    this.setState({ show: true });
-  };
-
-  handleFooterChange = () => {
-    if (this.state.footer === 0) {
-      return (
-        <Footer1
-          onChange={this.handleInputChange}
-          nextClick={this.nextClick}
-          getBaseFile={this.getBaseFile}
-        />
-      );
-    }
-    if (this.state.footer === 1) {
-      if (this.refs.html5) {
-        console.log(this.refs.html5);
-      }
-      return (
-        <Footer2
-          onChange={this.handleInputChange}
-          nextClick={this.nextClick}
-          previousClick={this.previousClick}
-          addProjectClick={this.addProjectClick}
-          isChecked={this.isChecked}
-          getBaseFile={this.getBaseFile}
-        />
-      );
-    }
-    if (this.state.footer === 2) {
-      if (!this.state.footerTruth) {
-        return (
-          <Footer3Button
-            cardButton={this.state.cardButton}
-            nextClick={this.nextClick}
-            previousClick={this.previousClick}
-            addProjectClick={this.addProjectClick}
-          />
-        );
-      } else if (this.state.footerTruth) {
-        return (
-          <Footer3
-            onChange={this.handleInputChange}
-            nextClick={this.nextClick}
-            previousClick={this.previousClick}
-            saveProjectButton={this.saveProjectButton}
-            getBaseFileProjectPic={this.getBaseFileProjectPic}
-          />
-        );
-      }
-    }
-    if (this.state.footer === 3) {
-      return (
-        <Footer4
-          onChange={this.handleInputChange}
-          nextClick={this.nextClick}
-          previousClick={this.previousClick}
-          serveLink={this.serveLink}
-          handleShow={this.handleShow}
-        />
-      );
-    }
-  };
-
   render() {
     return (
       <div>
         <Template1 {...this.state} />
         {this.state.footer}
-        {this.handleFooterChange()}
-        {this.renderRedirect()}
         {console.log(this.state.footer)}
-
-        <Modal show={this.state.show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>
-              Copy and Paste to Share your Profolio!
-            </Modal.Title>
-          </Modal.Header>
-          https://powerful-taiga-80042.herokuapp.com/profolio{this.state.servedLink}
-          <Modal.Footer>
-            <Button
-              variant="primary"
-              text="Close"
-              className="float-right medium buttonColor2 font-weight-bold btn active mr-1"
-              href="/usersplash"
-              onClick={this.handleClose}
-            />
-          </Modal.Footer>
-        </Modal>
       </div>
     );
   }
