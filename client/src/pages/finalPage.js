e6fcfa9536d893dfe07d93d3b9abf034e2076813
ylech <waltermelonson@gmail.com>
2019-07-29 09:16:15 -0700

    Testing for final page functionality

--- client/src/pages/finalPage.js
+++ client/src/pages/finalPage.js
@@ -44,43 +44,75 @@ class PortfolioCreation extends Component {
       match: { params }
     } = this.props;
 
-    // API.getUserStuff(${params.id}).then({data: profile})=>{
-    console.log(params.id);
-
     const code = (params.id + 1234) / 32 - 255;
 
     this.setState({ profolioId: code });
+
+    // API.getUserStuff(${params.id}).then({data: profile})=>{
+    console.log(params.id);
+    this.setState({ profolioId: params.id });
     // }
 
     let object = {
-      ProfolioId: code
+      ProfolioId: params.id
     };
 
     let bioObject = {};
+    let contactObject = {};
 
+    // *** GET ENTIRE PORTFOLIO FROM DB ***
     API.getPortfolio(object).then(response => {
       console.log("Response Data: " + response.data);
       let dataToSetState = [];
       if (response.data) {
         console.log(response.data);
 
-        // *** SKILLS ***
-        // console.log(response.data.Skills[0].skill);
+        // *** PARSE SKILLS ***
+        if (response.data.Skills.length > 0) {
+          console.log(response.data.Skills[0].skill);
 
-        // let loadedSkills = response.data.Skills[0].skill;
-        // console.log(loadedSkills.split(","));
-        // let skillsArray = loadedSkills.split(",");
-        // let skillsLinks = [];
-        // for (let i = 0; i < skillsArray.length; i++) {
-        //   skillsLinks.push("./images/template1/" + skillsArray[i] + ".png");
-        // }
-        // this.setState({
-        //   skills: skillsLinks
-        // });
+          let loadedSkills = response.data.Skills[0].skill;
+          console.log(loadedSkills.split(","));
+          let skillsArray = loadedSkills.split(",");
+          let skillsLinks = [];
+          for (let i = 0; i < skillsArray.length; i++) {
+            skillsLinks.push("./images/template1/" + skillsArray[i] + ".png");
+          }
+          this.setState({
+            skills: skillsLinks
+          });
+        }
 
         /////////////////////
 
-        // *** BIO ***
+        // *** PARSE PROJECTS ***
+
+        let projectCards = [];
+        let projectCard = {};
+
+        if (response.data.Projects.length > 0) {
+          console.log(response.data.Projects);
+          for (let i = 0; i < response.data.Projects.length; i++) {
+            projectCard = response.data.Projects[i];
+            let projectCardConverted = {
+              name: projectCard.name,
+              projectPicture: projectCard.thumbnail.toString(),
+              deployedLink: projectCard.deployedLink,
+              githubLink: projectCard.githubLink,
+              aboutProject: projectCard.aboutProject
+            };
+
+            projectCards.push(projectCardConverted);
+          }
+          console.log("ProjectCards: " + JSON.stringify(projectCards));
+          this.setState({
+            savedProject: projectCards
+          });
+        }
+
+        /////////////////////
+
+        // *** PARSE BIO AND CONTACT***
         bioObject = {
           table: "Bio",
           object: {
@@ -89,8 +121,14 @@ class PortfolioCreation extends Component {
             aboutMe: "aboutMe"
           }
         };
-        let contactArray = ["contact", "email", "github", "linkedIn"];
-        let finalArray = ["final", "finalLink"];
+        contactObject = {
+          table: "Contact",
+          object: {
+            phone: "contactNumber",
+            github: "githubProfileLink",
+            linkedIn: "linkedInLink"
+          }
+        };
 
         function parseData(dataObject) {
           let bioTest = response.data[dataObject.table];
@@ -117,6 +155,7 @@ class PortfolioCreation extends Component {
           }
         }
         parseData(bioObject);
+        parseData(contactObject);
       }
       console.log(dataToSetState);
       for (let i = 0; i < dataToSetState.length; i++) {
@@ -133,6 +172,8 @@ class PortfolioCreation extends Component {
       [initialKey]: initialValue
     });
   };
+
+  ////////////////////
 
   handleInputChange = event => {
     let value = event.target.value;
@@ -198,18 +239,41 @@ class PortfolioCreation extends Component {
         object = {
           fullName: this.state.fullName,
           aboutMe: this.state.aboutMe,
-          photo: this.state.baseImage.toString()
+          photo: this.state.baseImage.toString(),
+          ProfolioId: this.state.profolioId
         };
         API.saveBio(object);
         break;
       case 1:
-        object = {};
+        let clutteredSkills = this.state.skills;
+        console.log(clutteredSkills);
+        let cleanSkills = [];
+        for (let i = 0; i < clutteredSkills.length; i++) {
+          let lastSlash = clutteredSkills[i].lastIndexOf("/");
+          let lastDot = clutteredSkills[i].lastIndexOf(".");
+          let cleanSkill = clutteredSkills[i].substring(lastSlash + 1, lastDot);
+          cleanSkills.push(cleanSkill);
+        }
+        let cleanSkillsString = cleanSkills.join(",");
+        console.log(cleanSkillsString);
+        object = {
+          skill: cleanSkillsString,
+          ProfolioId: this.state.profolioId
+        };
+        API.saveSkills(object);
         break;
       case 2:
         object = {};
         break;
       case 3:
-        object = {};
+        object = {
+          email: this.state.userEmail,
+          phone: this.state.contactNumber,
+          github: this.state.githubProfileLink,
+          linkedIn: this.state.linkedInLink,
+          ProfolioId: this.state.profolioId
+        };
+        API.saveContact(object);
         break;
       default:
     }
@@ -252,7 +316,8 @@ class PortfolioCreation extends Component {
       thumbnail: this.state.projectPicture,
       deployedLink: this.state.deployedLink,
       githubLink: this.state.githubLink,
-      aboutProject: this.state.aboutProject
+      aboutProject: this.state.aboutProject,
+      ProfolioId: this.state.profolioId
     };
 
     API.saveProjectCard(savedProjectCard).then(async response => {
